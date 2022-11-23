e4b602eb0c296141ee736bd4de35c9afa05fe753
Nilansh Audichya <109611014+Nilansh1210@users.noreply.github.com>
2022-11-23 06:25:05 +0530

    Added Card component and it's CSS

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card(props) {
+    const classes  = 'class ' + props.className;
+  return (
+    
+    <div className={classes}>{props.children}</div>
+  )
+}
+
+export default Card
